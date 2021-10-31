fe7223281a95148f5bb31d64dd865bbf8e6bbdb1
InSeong-So <goflvhxj2547@gmail.com>
2021-10-31 19:47:22 +0900

    ✏️ useRef 정리

---
+++ 08_Hooks/hooks-tutorial/src/components/Average3.js
@@ -0,0 +1,42 @@
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+
+const getAverage = numbers => {
+  console.log('평균값 계산!');
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((a, b) => a + b);
+  return sum / numbers.length;
+}
+
+const Average = () => {
+  const [list, setList] = useState([]);
+  const [number, setNumber] = useState('');
+  const inputEl = useRef(null);
+  // 컴포넌트가 처음 렌더링 될 때만 함수 생성
+  const onChange = useCallback(({target}) => setNumber(target.value), []);
+  // number 또는 list가 바뀌었을 때만 함수 생성
+  const onInsert = useCallback(() => {
+    const nextList = list.concat(parseInt(number));
+    setList(nextList);
+    setNumber('');
+    inputEl.current.focus();
+  }, [number, list])
+
+  const avg = useMemo(() => getAverage(list), [list]);
+
+  return (
+    <div>
+      <input value={number} onChange={onChange} ref={inputEl}/>
+      <button onClick={onInsert}>등록</button>
+      <ul>
+        {list.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+      <div>
+        <b>평균값:</b> {avg}
+      </div>
+    </div>
+  )
+}
+
+export default Average;
